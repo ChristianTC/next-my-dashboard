1fe7feee044e62e3418998f70362d9349e950463
Christian Tambo <christiantambo15@gmail.com>
2025-02-01 20:18:40 -0400

    implementar useState

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,9 +1,35 @@
+'use client'
+import { Metadata } from "next";
+import { useState } from "react";
+
+export const metadata: Metadata = {
+  title: 'Counter',
+  description: 'Contador Client Side'
+}
 
 export default function CounterPage() {
 
+  const [counter, setCounter] = useState(0)
+
   return (
-    <div>
-      <h1>Hello Counter</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos del carrito</span>
+      <span className="text-9xl"> {counter} </span>
+
+      <div className="flex">
+        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={()=>setCounter(counter+1)}
+        >
+          +1
+        </button>
+
+        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={()=>setCounter(counter-1)}
+        >
+          -1
+        </button>
+      </div>
+
     </div>
   );
 }
