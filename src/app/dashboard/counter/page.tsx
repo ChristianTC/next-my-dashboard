726f6e3abecdee0aad3550b2dda27ce2d8e8ccc1
Christian Tambo <christiantambo15@gmail.com>
2025-02-01 20:25:29 -0400

    crear componentes pequenos para integrar al client components con server components, Single Responsability Principle

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,34 +1,17 @@
-'use client'
+import { CartCounter } from "@/app/shopping-cart";
 import { Metadata } from "next";
-import { useState } from "react";
 
 export const metadata: Metadata = {
-  title: 'Counter',
-  description: 'Contador Client Side'
+  title: 'Shopping Cart',
+  description: 'Contador'
 }
 
 export default function CounterPage() {
 
-  const [counter, setCounter] = useState(0)
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos del carrito</span>
-      <span className="text-9xl"> {counter} </span>
-
-      <div className="flex">
-        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={()=>setCounter(counter+1)}
-        >
-          +1
-        </button>
-
-        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={()=>setCounter(counter-1)}
-        >
-          -1
-        </button>
-      </div>
+      <CartCounter value={10} />
 
     </div>
   );
